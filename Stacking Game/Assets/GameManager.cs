5ab119680160fb8e14f3771b3891f2891d098a3f
tacjoseph <potatoesking0@gmail.com>
2020-03-13 08:41:36 -0400

    Added variables to gamemanager

---
+++ Stacking Game/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver;
+    public bool collideWithGround = false;
+    public int moveCount = 0;
+    public Transform crateSpawnPos;
+    public Vector3 cameraTargetPos;
+    public Text bestScore;
+    public Text scoreText;
+    public int score = 0;
+    public GameObject gameOverText;
+    //Crate crate
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Stacking Game/Assets/Scoretracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoretracker : MonoBehaviour
+{
+    public Scoretracker INSTANCE;
+    public int bestScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(INSTANCE == null){
+            INSTANCE = this;
+            bestScore = 0;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
